eef7d90aac6057bc5115355918b1fccfa1211da0
Raul de Arriba <rauldearriba@gmail.com>
2024-03-18 07:09:05 +0100

    add nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,22 @@ import * as joi from 'joi';
 interface EnvVars {
     PORT: number;
     DATABASE_URL: string;
+
+    NATS_SERVERS: string[];
 }
 
 const ensSchema = joi.object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
 })
 .unknown(true)
 
-const {error, value} = ensSchema.validate(process.env);
+const {error, value} = ensSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 if ( error ) {
     throw new Error(`Config validation error: ${ error.message }`);
@@ -23,4 +30,6 @@ const envVars: EnvVars = value;
 export const envs = {
     port: envVars.PORT,
     databaseUrl: envVars.DATABASE_URL,
+
+    natServers: envVars.NATS_SERVERS,
 }
